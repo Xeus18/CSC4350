2f53480ecc56a16c8086e19f90295d59b2e9b377
JT <jbenson04@aurora.edu>
2019-09-21 13:11:37 -0500

    Testing pycharm Github connection

--- Car.py
+++ Car.py
@@ -1,3 +1,18 @@
-print("Hello")
-print("Welcome")
-print("hello test")
+class Car:
+#Created by Jorge
+    def __init__(self,speed, started):
+
+        self.speed = 0
+        self.started = False
+
+    def start(self,started, speed):
+        self.started = True
+        self.speed= 0
+
+    def isStarted(self):
+        return(Car.started)
+
+
+
+
+
