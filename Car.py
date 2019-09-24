cb5010f3998fba16eeead07ba29b2b056c66f829
JT <jbenson04@aurora.edu>
2019-09-24 08:40:49 -0500

    Jatrence

--- Car.py
+++ Car.py
@@ -1,18 +1,37 @@
-class Car:
-#Created by Jorge
-    def __init__(self,speed, started):
 
+class Vehicle(object):
+    """docstring"""
+
+    def __init__(self, color, doors, tires, vtype,speed):
+        """Constructor"""
+        self.color = color
+        self.doors = doors
+        self.tires = tires
+        self.vtype = vtype
         self.speed = 0
-        self.started = False
 
-    def start(self,started, speed):
-        self.started = True
-        self.speed= 0
+    def brake(self):
+        """
+        Stop the car
+        """
+        return "%s braking" % self.vtype
 
-    def isStarted(self):
-        return(Car.started)
+    def drive(self,speed):
+        """
+        Drive the car
+        """
+        speed +=5
+        return "I'm driving a %s %s! %s" % (self.color, self.vtype,self.speed)
 
 
+if __name__ == "__main__":
+    truck = Vehicle("red", 3, 6, "truck",0)
+    print(truck.drive())
+    print(truck.brake())
+
+    car = Vehicle("blue", 5, 4, "car")
+    print(car.brake())
+    print(car.drive())
 
 
 
