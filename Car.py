0d9ee49e10658abab3a3a97784d0b00667e1ce06
jorge <jbaeza01@aurora.edu>
2019-09-24 09:10:29 -0500

    updated Car.py to work

--- Car.py
+++ Car.py
@@ -1,6 +1,7 @@
 
 class Vehicle(object):
     """docstring"""
+    speed = 0
 
     def __init__(self, color, doors, tires, vtype, speed):
         """Constructor"""
@@ -8,30 +9,31 @@ class Vehicle(object):
         self.doors = doors
         self.tires = tires
         self.vtype = vtype
-        self.speed = 0
+        self.speed = speed
 
     def brake(self):
         """
         Stop the car
         """
-        return "%s braking" % self.vtype
+        Vehicle.speed = 0
+        return "%s braking" % Vehicle.speed
 
-    def drive(self,speed):
+    def drive(self, speed):
         """
         Drive the car
         """
-        speed +=5
-        return "I'm driving a %s %s! %s" % (self.color, self.vtype,self.speed)
+        Vehicle.speed += 5
+        return "I'm driving a %s %s! %s" % (self.color, self.vtype, Vehicle.speed)
 
 
 if __name__ == "__main__":
-    truck = Vehicle("red", 3, 6, "truck", speed)
-    print(truck.drive())
+    truck = Vehicle("red", 3, 6, "truck", str(Vehicle.speed))
+    print(truck.drive(Vehicle.speed))
     print(truck.brake())
 
-    car = Vehicle("blue", 5, 4, "car")
+    car = Vehicle("blue", 5, 4, "car", str(Vehicle.speed))
     print(car.brake())
-    print(car.drive())
+    print(car.drive(Vehicle.speed))
 
 
 
